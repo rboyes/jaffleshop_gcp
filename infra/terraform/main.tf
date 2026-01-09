8e1befd1c593cde397859a93995130153eeb7a6b
Rich Boyes <rich.boyes@admiralpioneer.com>
2026-01-09 18:01:25 +0000

    fix: x

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -8,6 +8,16 @@ import {
   id = "projects/${var.project_id}/serviceAccounts/dbt-runner@${var.project_id}.iam.gserviceaccount.com"
 }
 
+import {
+  to = google_bigquery_dataset.prod
+  id = "projects/${var.project_id}/datasets/${var.prod_dataset_id}"
+}
+
+import {
+  to = google_bigquery_dataset.dev
+  id = "projects/${var.project_id}/datasets/${var.dev_dataset_id}"
+}
+
 resource "google_project_service" "bigquery" {
   project = var.project_id
   service = "bigquery.googleapis.com"
